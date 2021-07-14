7713ae6f0cc354092e64ea13e7996dec8bef8a20
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-07-14 15:51:47 +0530

    update0

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO
 import pymongo
 import requests
 from constants import adhiAtlasAdminPassword
 from bson.json_util import dumps
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+
+app = Flask(__name__, template_folder="./Frontend", static_folder="./static/")
+
+app.config["SECRET_KEY"] = "secret!"
+app.debug = True
+
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    socketio.run(app)
-
-connectionURL = "mongodb+srv://adhiAtlasAdmin:%s@cluster0.cjyig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"%adhiAtlasAdminPassword
+connectionURL = (
+    "mongodb+srv://adhiAtlasAdmin:%s@cluster0.cjyig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    % adhiAtlasAdminPassword
+)
 client = pymongo.MongoClient(connectionURL)
-db = client.get_database('Reception')
+db = client.get_database("Reception")
 
-# print("Working till here")\
 
-@app.route('/manage', methods=['POST'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/manage", methods=["POST"])
 def getFromDB():
     queryOutput = db.Rooms.find({})
     return dumps(queryOutput)
 
-
-
-
-
-
-
+if __name__ == "__main__":
+    socketio.run(app)
