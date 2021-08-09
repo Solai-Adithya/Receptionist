ac728430f8c599b73995e0790e3889ca31c6544c
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-08-09 23:51:47 +0530

    Added real time update in HTML

--- functions.py
+++ functions.py
@@ -8,8 +8,9 @@ from constants import EMAIL_ID, EMAIL_PASSWORD
 from email.message import EmailMessage
 from db import User, Rooms
 
+
 def generateRoomID():
-    roomID = "".join([chr(random.randint(97, 122)) for _ in range(5)])
+    roomID = "".join([chr(random.randint(97, 122)) for _ in range(16)])
     return roomID
 
 
@@ -23,27 +24,34 @@ def authenticated_only(f):
 
     return wrapped
 
+
 def invite_user(room_id, participant_email):
     # Ask user to join the room immediately
     room = Rooms.getRoomByID(room_id)
     room_name = room["name"]
     interviewer_email = room["creator"]
     meet_link = room["meeting_link"]
-    interviewer_name = User.getUserByEmail(interviewer_email, {"name":1})["name"]
+    interviewer_name = User.getUserByEmail(interviewer_email, {"name": 1})[
+        "name"
+    ]
     msg = EmailMessage()
-    msg['Subject'] = '%s invited you to join the meeting. Join Now!'%interviewer_name
-    msg['From'] = EMAIL_ID
-    msg['To'] = participant_email
+    msg["Subject"] = (
+        "%s invited you to join the meeting. Join Now!" % interviewer_name
+    )
+    msg["From"] = EMAIL_ID
+    msg["To"] = participant_email
 
-    msg.set_content(f"""
+    msg.set_content(
+        f"""
     {interviewer_name} invited you to join the {room_name} meeting using the below link right now. Please join as soon as possible.
     Meeting Link: {meet_link}
 
     On behalf of {interviewer_name} ({interviewer_email}),
     Receptionist.tech
-    """)
+    """
+    )
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(EMAIL_ID, EMAIL_PASSWORD)
         smtp.send_message(msg)
 
@@ -54,13 +62,16 @@ def notify_user(room_id, participant_email):
     room_name = room["name"]
     interviewer_email = room["creator"]
     meet_link = room["meeting_link"]
-    interviewer_name = User.getUserByEmail(interviewer_email, {"name":1})["name"]
+    interviewer_name = User.getUserByEmail(interviewer_email, {"name": 1})[
+        "name"
+    ]
     msg = EmailMessage()
-    msg['Subject'] = 'You are up next. Get ready!'
-    msg['From'] = EMAIL_ID
-    msg['To'] = participant_email
+    msg["Subject"] = "You are up next. Get ready!"
+    msg["From"] = EMAIL_ID
+    msg["To"] = participant_email
 
-    msg.set_content(f"""
+    msg.set_content(
+        f"""
     You are the next person in {room_name}'s participant queue. You can find the meeting link below. Get ready! 
     Meeting Link: {meet_link}
 
@@ -68,8 +79,9 @@ def notify_user(room_id, participant_email):
 
     On behalf of {interviewer_name} ({interviewer_email}),
     Receptionist.tech
-    """)
+    """
+    )
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(EMAIL_ID, EMAIL_PASSWORD)
         smtp.send_message(msg)
