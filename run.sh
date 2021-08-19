a0bca719804df7c9d313917e35a661cafd903e85
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-08-19 16:19:13 +0530

    fixed exit of script

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
+set -e
+
+# https://unix.stackexchange.com/questions/55558/how-can-i-kill-and-wait-for-background-processes-to-finish-in-a-shell-script-whe
+
+trap 'killall' INT
+
+killall() {
+    trap '' INT TERM
+    echo "****...Shutting Down...****"
+    kill -TERM 0
+    wait
+    echo DONE
+}
 
 python3 main.py &
-
+echo $!
 python3 watch_stream.py & 
+echo $!
+
+cat
